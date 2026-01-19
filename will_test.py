091b9f544fa4ebd4f75065416719bb50a4dc4795
WillM2006 <rh24922@bristol.ac.uk>
2026-01-19 15:51:24 +0000

    huedbd

---
+++ will_test.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+# Create figure and axis
+fig, ax = plt.subplots()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+# Ball properties
+x, y = 5, 5
+vx, vy = 0.1, 0.15
+
+ball, = ax.plot([], [], 'ro', markersize=12)
+
+def init():
+    ball.set_data([], [])
+    return ball,
+
+def update(frame):
+    global x, y, vx, vy
+
+    x += vx
+    y += vy
+
+    # Bounce off walls
+    if x <= 0 or x >= 10:
+        vx *= -1
+    if y <= 0 or y >= 10:
+        vy *= -1
+
+    ball.set_data(x, y)
+    return ball,
+
+ani = animation.FuncAnimation(
+    fig,
+    update,
+    init_func=init,
+    frames=300,
+    interval=20,
+    blit=True
+)
+
+plt.show()
